b8299384491a2b4a292934765d95b103d8ae7673
Nikolay <repyah84@gmail.com>
2023-07-16 10:33:22 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
-import { ButtonToggleModule } from '@nikorg/my-button-toggle';
 import { FormsModule } from '@angular/forms';
+import { ButtonToggleModule } from '@nikolay_ua_mk/button-toggle';
 
 @NgModule({
   declarations: [AppComponent],

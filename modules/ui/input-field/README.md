348975aa5a114ced73c05283a0bbc25421f64958
Nikolay <repyah84@gmail.com>
2023-07-18 14:17:41 +0300

    implement input field lib

---
+++ modules/ui/input-field/README.md
@@ -0,0 +1,7 @@
+# modules-ui-input-field
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test modules-ui-input-field` to execute the unit tests.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ButtonToggleModule } from '@nikolay_ua_mk/button-toggle';
+import { InputFieldModule } from '@nikolay_ui_mk/input-field';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ButtonToggleModule],
+  imports: [BrowserModule, ButtonToggleModule, InputFieldModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
